fd442d284ca57acd84c06292a52a10512e8377c8
Nicholas Lim <nicknitewolf@lineageos.org>
2022-06-09 23:18:36 +0800

    Update the targets I actually want gms on

--- BoardConfigExtra.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(WITH_TWRP),true)
-TW_EXCLUDE_SUPERSU := true
-TW_EXCLUDE_TWRPAPP := true
-endif
--- product.mk
+++ product.mk
@@ -1,11 +1,6 @@
-ifneq ($(filter lineage_marlin lineage_beryllium lineage_tissot ,$(TARGET_PRODUCT)),)
-# TARGET_ARCH doesn't get set in time, so let's make sure its done before we inherit gapps
-TARGET_ARCH := arm64
--include vendor/gapps/$(TARGET_ARCH)/$(TARGET_ARCH)-vendor.mk
-endif
-
-ifneq ($(filter lineage_wt88047 ,$(TARGET_PRODUCT)),)
-# TARGET_ARCH doesn't get set in time, so let's make sure its done before we inherit gapps
-TARGET_ARCH := arm
--include vendor/gapps/$(TARGET_ARCH)/$(TARGET_ARCH)-vendor.mk
-endif
+ifneq ($(filter lineage_q6x ,$(TARGET_PRODUCT)),)
+ifeq ($(WITH_GMS),true)
+WITH_GMS_TV := true
+GMS_MAKEFILE := gms_minimal.mk
+endif #WITH_GMS
+endif #TARGET_PRODUCT

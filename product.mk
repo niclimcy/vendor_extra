dbb2832bb0710e9da2d275f6f746fbb1d4c59fe1
Nicholas Lim <nicknitewolf@lineageos.org>
2023-05-14 01:24:24 +0800

    The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- product.mk
+++ product.mk
@@ -2,3 +2,9 @@
 ifeq ($(WITH_GMS), true)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
+
+# Google Camera
+ifneq ($(filter lineage_lynx ,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES += \
+    vendor/extra/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+endif #TARGET_PRODUCT

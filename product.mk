ecb84bd3a03c59e74aa1595b19cdb3720a1d2d2d
Nicholas Lim <nicknitewolf@lineageos.org>
2023-11-25 22:46:34 +0800

    ih8sn
    Change-Id: I9e7e8a9fa75b23825941bfd64e88c2747f2e84cd

--- product.mk
+++ product.mk
@@ -6,5 +6,9 @@ endif
 # Google Camera
 ifneq ($(filter lineage_lynx ,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
-    vendor/extra/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+    vendor/extra/etc/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml \
+    vendor/extra/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += system/bin/ih8sn system/etc/ih8sn.conf system/etc/init/ih8sn.rc
+PRODUCT_PACKAGES += ih8sn
 endif #TARGET_PRODUCT

1ac6605240ac547227e8a11f735b74fb892959c9
Nicholas Lim <nicknitewolf@lineageos.org>
2024-11-22 00:12:55 +0800

    Remove ih8sn

--- product.mk
+++ product.mk
@@ -6,9 +6,5 @@ endif
 # Google Camera
 ifneq ($(filter lineage_lynx ,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
-    vendor/extra/etc/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml \
-    vendor/extra/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += system/bin/ih8sn system/etc/ih8sn.conf system/etc/init/ih8sn.rc
-PRODUCT_PACKAGES += ih8sn
+    vendor/extra/etc/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 endif #TARGET_PRODUCT
